79e7250cffe133dd4b155c9af663a9bfafd314a5
dangvanmih <minhmeo2k413@gmail.com>
2026-02-17 23:53:36 +0700

    install pug

--- Node-js/Bai-1/index.js
+++ Node-js/Bai-1/index.js
@@ -1,12 +1,28 @@
+// lệnh chạy server: npx nodemon index.js
 const express = require('express')
 const app = express();
-const port = 3000
+const port = 3000;
+
+app.set("views","./views");
+app.set("view engine", "pug");
 
 app.get('/',(req, res) => {
-    res.send('Hello World')
+    res.render("index.pug", {
+        title: "Trang chủ",
+        message: "Xin chào các bạn"
+    })
+})
+
+app.get('/product',(req, res) => {
+    res.send('<h1>Product</h1>')
+})
+
+app.get('/about',(req, res) => {
+    res.send('<h1>Liên hệ</h1>')
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
     
 })
+
