50341faf9d1b0a5d9ce8cf20fcc6e56afa513acf
dangvanmih <minhmeo2k413@gmail.com>
2026-02-25 15:30:54 +0700

    Lấy data ra giao diện product admin

--- Product-management/controller/admin/product-controller.js
+++ Product-management/controller/admin/product-controller.js
@@ -1,6 +1,14 @@
+const Product = require("../../models/products.model")
+
 //[GET] /admin/products
-module.exports.products = (req, res) => {
+module.exports.products = async (req, res) => {
+    const products = await Product.find({
+      deleted: false
+    })
+    // console.log(products);
+    
     res.render("admin/pages/products/index", {
       pageTitle: "Trang sản phẩm",
+      products: products
     })
   }
