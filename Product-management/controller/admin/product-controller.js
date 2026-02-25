87520aef34bd0fde07e2db5d6b8387116bc92dd2
dangvanmih <minhmeo2k413@gmail.com>
2026-02-25 18:24:52 +0700

    Tính năng tìm kiếm sản phẩm

--- Product-management/controller/admin/product-controller.js
+++ Product-management/controller/admin/product-controller.js
@@ -3,44 +3,54 @@ const Product = require("../../models/products.model")
 //[GET] /admin/products
 module.exports.products = async (req, res) => {
   // console.log(req.query.status);
-    let fillterStatus = [
-      {
-        name: "Tất cả",
-        status: "",
-        class: ""
-      },
-      {
-        name: "Hoạt động",
-        status: "active"
-      },
-      {
-        name: "Dừng hoạt động",
-        status: "inactive"
-      },
-    ]
-    let find = {
-      deleted: false
-    };
-
-    if(req.query.status) {
-      const index = fillterStatus.findIndex(item => item.status == req.query.status ) // lặp qua từng bản ghi của fillterStatus và lấy ra item.status == req.query.status 
-      fillterStatus[index].class = "active"
-    }
-    else {
-      const index = fillterStatus.findIndex(item => item.status == "") // lặp qua từng bản ghi của fillterStatus và lấy ra item.status == rỗng
-      fillterStatus[index].class = "active"
-    }
-    
-    if(req.query.status) // check nếu người dùng lọc thì mới thêm key status vào hàm find
+  let fillterStatus = [
     {
-      find.status = req.query.status; // thêm key status  và gán câu truy vấn
-    };
-    const products = await Product.find(find);
-    // console.log(products);
-    
-    res.render("admin/pages/products/index", {
-      pageTitle: "Trang sản phẩm",
-      products: products,
-      fillterStatus: fillterStatus
-    });
+      name: "Tất cả",
+      status: "",
+      class: ""
+    },
+    {
+      name: "Hoạt động",
+      status: "active"
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive"
+    },
+  ]
+  let find = {
+    deleted: false
+  };
+
+  if (req.query.status) {
+    const index = fillterStatus.findIndex(item => item.status == req.query.status) // lặp qua từng bản ghi của fillterStatus và lấy ra item.status == req.query.status 
+    fillterStatus[index].class = "active"
   }
+  else {
+    const index = fillterStatus.findIndex(item => item.status == "") // lặp qua từng bản ghi của fillterStatus và lấy ra item.status == rỗng
+    fillterStatus[index].class = "active"
+  }
+
+  if (req.query.status) // check nếu người dùng lọc thì mới thêm key status vào hàm find
+  {
+    find.status = req.query.status; // thêm key status  và gán câu truy vấn
+  };
+  
+  let keyword = "";
+  if (req.query.keyword) // check nếu người dùng tìm kiếm
+  {
+    keyword = req.query.keyword;
+    const regex = new RegExp(keyword, "i"); // dùng regex để tìm kiếm khi không nhập đủ tên sản phẩm và tham số "i" để tìm kiếm ko phân biệt chữ in hoa
+    find.title = regex;
+  };
+
+  const products = await Product.find(find);
+  // console.log(products);
+
+  res.render("admin/pages/products/index", {
+    pageTitle: "Trang sản phẩm",
+    products: products,
+    fillterStatus: fillterStatus,
+    keyword: keyword
+  });
+}
--- Product-management/public/admin/js/script.js
+++ Product-management/public/admin/js/script.js
@@ -2,14 +2,12 @@
 
 const buttonStatus = document.querySelectorAll("[button-status]"); // để lấy các thuộc tính tự định nghĩa thì bọc bởi []
 //check xem nếu lấy ra được các thẻ rồi thì mới xử lý logic
-if(buttonStatus.length > 0)
-{
+if (buttonStatus.length > 0) {
   let url = new URL(window.location.href); // lấy ra url bằng hàm URL và phải dùng hàm URL thì với dùng được hàm searchParams.set để set lại params
   buttonStatus.forEach(button => {
     button.addEventListener("click", () => {
       const status = button.getAttribute("button-status");
-      if(status)
-      {
+      if (status) {
         url.searchParams.set("status", status); // hàm searchParams là lấy url hiện tại và set thêm các key phía sau
       }
       else {
@@ -20,6 +18,24 @@ if(buttonStatus.length > 0)
     })
   });
 }
-
-
 // end-button status
+
+//form search
+
+const formSearch = document.querySelector("#form-search")
+if (formSearch) {
+  let url = new URL(window.location.href) // vẫn phải thêm url tại vì khi tìm kiếm theo trạng thái thì bắt buộc phải lấy url hiện tại rồi mới tìm kiếm
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const value = e.target.elements.keyword.value;
+    if (value) {
+        url.searchParams.set("keyword", value); // hàm searchParams là lấy url hiện tại và set thêm các key phía sau
+      }
+      else {
+        url.searchParams.delete("keyword");
+      }
+      window.location.href = url.href // chuyển hướng sang trang khác
+  })
+}
+
+//end-form seacrh
