abde8a3c551761214bad59fa68d4783f8207ce23
dangvanmih <minhmeo2k413@gmail.com>
2026-02-26 21:28:21 +0700

    tối ưu tính năng search

--- Product-management/controller/admin/product-controller.js
+++ Product-management/controller/admin/product-controller.js
@@ -1,6 +1,8 @@
 const Product = require("../../models/products.model")
 
 const filterStatusHelper = require("../../helpers/filterStatus");
+
+const seacrhHelper = require("../../helpers/search")
 //[GET] /admin/products
 module.exports.products = async (req, res) => {
   // console.log(req.query.status);
@@ -12,20 +14,19 @@ module.exports.products = async (req, res) => {
     deleted: false
   };
   
-  if (req.query.status) // check nếu người dùng lọc thì mới thêm key status vào hàm find
+  if (req.query.status) // check nếu người dùng lọc thì mới thêm key status vào object find
   {
     find.status = req.query.status; // thêm key status  và gán câu truy vấn
   };
   // hết bộ lọc
 
   // tìm kiếm
-  let keyword = "";
-  if (req.query.keyword) // check nếu người dùng tìm kiếm
-  {
-    keyword = req.query.keyword;
-    const regex = new RegExp(keyword, "i"); // dùng regex để tìm kiếm khi không nhập đủ tên sản phẩm và tham số "i" để tìm kiếm ko phân biệt chữ in hoa
-    find.title = regex;
-  };
+  const objectSearch = seacrhHelper(req.query);
+
+  if(objectSearch.regex) {
+    find.title = objectSearch.regex;
+  }
+  
   // hết tìm kiếm
 
 
@@ -36,6 +37,6 @@ module.exports.products = async (req, res) => {
     pageTitle: "Trang sản phẩm",
     products: products,
     filterStatus: filterStatus,
-    keyword: keyword
+    keyword: objectSearch.keyword
   });
 }
