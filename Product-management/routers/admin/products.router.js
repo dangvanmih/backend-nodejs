9a79e77855e147fa9337d7977f2c0315fa3779e6
dangvanmih <minhmeo2k413@gmail.com>
2026-03-04 15:08:35 +0700

    xóa vĩnh viễn sản phẩm

--- Product-management/routers/admin/products.router.js
+++ Product-management/routers/admin/products.router.js
@@ -6,4 +6,7 @@ const controller = require("../../controller/admin/product-controller")
   router.patch('/change-status/:status/:id', controller.changeStatus);
 
   router.patch("/change-multi-status", controller.changeMulti);
+
+  router.delete('/delete/:id', controller.deleteItem);
+
   module.exports = router;
