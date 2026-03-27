4fd96b121461cae4d35fdcbb6fc93419a674c5d3
dangvanmih <minhmeo2k413@gmail.com>
2026-03-27 18:25:08 +0700

    fix Lỗi số thứ tự

--- Product-management/helpers/createTree.js
+++ Product-management/helpers/createTree.js
@@ -1,15 +1,22 @@
-const createTree = (arr, parentId = "") => {
+const createTree = (arr, parentId = "", state = { count: 0 }) => {
   const tree = [];
+
   arr.forEach((item) => {
     if (item.parent_id === parentId) {
+      state.count++;
       const newItem = item;
-      const children = createTree(arr, item.id); // ✅ OK
+      newItem.index = state.count;
+
+      const children = createTree(arr, item.id, state);
+
       if (children.length > 0) {
         newItem.children = children;
       }
+
       tree.push(newItem);
     }
   });
+
   return tree;
 };
 
