895529a9256f2efe7376a4c86bf0c06f7d5d6c07
dangvanmih <minhmeo2k413@gmail.com>
2026-04-03 13:31:52 +0700

    Tính năng đăng xuất

--- Product-management/routers/admin/auth.router.js
+++ Product-management/routers/admin/auth.router.js
@@ -4,7 +4,6 @@ const validates = require("../../validates/admin/auth.validate")
 const controller = require("../../controller/admin/auth-controller")
 
   router.get('/login', controller.login);
-  
   router.post('/login', validates.loginPost, controller.loginPost);
-
+  router.get('/logout', controller.logout);
 module.exports = router;
