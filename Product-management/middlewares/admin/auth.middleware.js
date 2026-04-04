858197b6b5e6d7a2cbcf10ec53873fb39051df73
dangvanmih <minhmeo2k413@gmail.com>
2026-04-04 09:21:20 +0700

    thêm authorization

--- Product-management/middlewares/admin/auth.middleware.js
+++ Product-management/middlewares/admin/auth.middleware.js
@@ -1,7 +1,7 @@
 const systemConfig = require("../../config/system");
 const accounts = require("../../models/accounts.model");
+const roles = require("../../models/roles.model");
 module.exports.requireAuth = async (req, res, next) => {
-  console.log(req.cookies.token);
   const token = req.cookies.token
   if (!token) {
     res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
@@ -9,12 +9,19 @@ module.exports.requireAuth = async (req, res, next) => {
   else {
     const user = await accounts.findOne({
       token: token
-    });
+    }).select("-password");
     if (!user) {
       res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     }
     else {
+      const role = await roles.findOne({
+        _id:user.role_id
+      }).select("title permission")
+      
+      res.locals.user = user
+      res.locals.role = role
       next();
     }
   }
 }
+// 54:04
