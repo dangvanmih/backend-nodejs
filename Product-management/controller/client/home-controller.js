9a3e647a42f5897409ead2e419769807a025a8b3
dangvanmih <minhmeo2k413@gmail.com>
2026-04-06 23:32:41 +0700

    vibe code trang home

--- Product-management/controller/client/home-controller.js
+++ Product-management/controller/client/home-controller.js
@@ -1,8 +1,16 @@
+const Product = require("../../models/products.model");
 //[GET] /
 module.exports.index = async (req, res) => {
-  
+  //lấy ra sản phẩm nổi bật
+  let find = {
+    deleted: false,
+    featured: "1",
+    status: "active"
+  }
+  const productFeatured = await Product.find(find)
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-
+    productFeatured: productFeatured
   })
 }
