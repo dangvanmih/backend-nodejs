41f92da1dd11dbbfb5b78c584b6c0a8bd9d49168
dangvanmih <minhmeo2k413@gmail.com>
2026-04-06 21:49:12 +0700

    thêm đoạn code check phân quyền bên BE admin

--- Product-management/controller/admin/products-category-controller.js
+++ Product-management/controller/admin/products-category-controller.js
@@ -70,6 +70,16 @@ module.exports.create = async (req, res) => {
 
 //[POST] /admin/products-category/createPost
 module.exports.createPost = async (req, res) => {
+  
+  // const permissions = res.locals.role.permission;
+  // if(permissions.includes("product-category_create")) {
+  //   // copy code bên dưới vào đây!
+  //   //đoạn code này để check ngăn ko cho postman xử lý vào, còn mới chỉ xử lý bên FE
+  // }
+  // else {
+  //   return
+  // }
+
   if (req.body.position === "") {
     const count = await productsCategory.countDocuments();
     req.body.position = count + 1;
