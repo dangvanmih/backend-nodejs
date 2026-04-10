606fb39080a0e6c392b5df79f82b29d0c4a19e54
dangvanmih <minhmeo2k413@gmail.com>
2026-04-10 16:04:08 +0700

    Hiển thị số lượng sản phẩm ở miniCart

--- Product-management/middlewares/client/cart.middleware.js
+++ Product-management/middlewares/client/cart.middleware.js
@@ -1,17 +1,26 @@
 const Cart = require("../../models/cart.model")
 module.exports.cartId = async (req, res, next) => {
-  if(!req.cookies.cartId) {
+  if (!req.cookies.cartId) {
     // khi chưa có giỏ hàng
     const cart = new Cart()
     await cart.save();
     const expiresTime = 1000 * 60 * 60 * 24 * 365
-    console.log(cart);
-    res.cookie("cartId", cart.id,{
+
+    res.cookie("cartId", cart.id, {
       expires: new Date(Date.now() + expiresTime)
     });
   }
   else {
-    // khi có giỏ hàng
+    const cart = await Cart.findOne({
+      _id: req.cookies.cartId
+    })
+    // // Đếm xem có bao nhiêu hàng (loại sản phẩm) trong giỏ
+    // cart.totalProducts = cart.products.length;
+
+    // đếm số lượng quantity
+    cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0)
+
+    res.locals.miniCart = cart;
   }
   next();
 }
