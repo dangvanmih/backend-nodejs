646ec31f8ee65a72e7d631570f07560613165596
dangvanmih <minhmeo2k413@gmail.com>
2026-04-17 13:11:43 +0700

    Tính năng chat cơ bản

--- Product-management/controller/client/chat-controller.js
+++ Product-management/controller/client/chat-controller.js
@@ -1,13 +1,38 @@
+const Chat = require("../../models/chats.model");
+const User = require("../../models/users.model");
 //[GET]/chat/
 module.exports.index = async (req, res) => {
+  const userId = res.locals.user.id;
 
   // SocketIo 
-  _io.on('connection', (socket) => {
-    console.log('a user connected');
+  _io.once('connection', (socket) => {
+    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+      // lưu vào DB
+      const chat = new Chat({
+        user_id: userId,
+        content: content
+      })
+      await chat.save();
+    });
   });
 
   //End socketIo
+
+
+  //Lấy ra data
+  const chats = await Chat.find({
+    deleted: false
+  });
+  for (const chat of chats) {
+    const infoUser = await User.findOne({
+      _id: chat.user_id
+    }).select("fullName")
+
+    chat.infoUser = infoUser
+  }
+  
   res.render("client/pages/chat/index", {
-    pageTitle: "Chat"
+    pageTitle: "Chat",
+    chats: chats
   })
 }
